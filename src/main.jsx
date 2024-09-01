d972bec1f631aea8b12ab65075acfdbef0f524c4
Kenneth sunjaya <kennethsunjaya@gmail.com>
2024-09-02 01:14:02 +0700

    updated title and icon

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ContextProvider } from './provider/Provider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </StrictMode>,
+  <ContextProvider>
+    <App />
+  </ContextProvider>
 )
